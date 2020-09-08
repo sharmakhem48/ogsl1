e7410703a5ebbc910776254ec693713e36ca0cf6
sharmakhem48 <sharmakhem48@gmail.com>
2020-09-08 21:43:12 +0530

    Adding new build

---
+++ build/precache-manifest.bdf4a345f9344874276b48e21710c369.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e39f5ddd2cc880b0cfeeed12c06879",
+    "url": "/ogsl/index.html"
+  },
+  {
+    "revision": "949003fd5d08a8d5588f",
+    "url": "/ogsl/static/css/main.a23abb56.chunk.css"
+  },
+  {
+    "revision": "7faf1c90825c93fe0ed9",
+    "url": "/ogsl/static/js/2.5f93f519.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/ogsl/static/js/2.5f93f519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "949003fd5d08a8d5588f",
+    "url": "/ogsl/static/js/main.c8ff2b9d.chunk.js"
+  },
+  {
+    "revision": "99377591648e5a3277b2",
+    "url": "/ogsl/static/js/runtime-main.52ce04a8.js"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/ogsl/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/ogsl/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/ogsl/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/ogsl/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "a313eddfb3dcbc2898f9d208053483d2",
+    "url": "/ogsl/static/media/ogsl_1.a313eddf.jpg"
+  },
+  {
+    "revision": "92c27a233c9703901424b1423abfcb33",
+    "url": "/ogsl/static/media/ogsl_2.92c27a23.jpg"
+  },
+  {
+    "revision": "ac7d6e369c5d5db7e3426258bdc407ad",
+    "url": "/ogsl/static/media/ogsl_3.ac7d6e36.jpg"
+  },
+  {
+    "revision": "cbf8803996f1e5df16aa61b310e66d1b",
+    "url": "/ogsl/static/media/ogsl_4.cbf88039.jpg"
+  },
+  {
+    "revision": "724f11d8b5e7a44f9388d0264b13cabd",
+    "url": "/ogsl/static/media/ogsl_5.724f11d8.jpg"
+  },
+  {
+    "revision": "bd409438faa15c9c2caf6cc638cf100d",
+    "url": "/ogsl/static/media/ogsl_6.bd409438.jpg"
+  },
+  {
+    "revision": "4a695f71035e66216975e639725d7853",
+    "url": "/ogsl/static/media/ogsl_8.4a695f71.jpg"
+  }
+]);
