c839c620502658ab3167b33ab81f5e8a0845e98a
sharmakhem48 <sharmakhem48@gmail.com>
2021-01-23 17:57:21 +0530

    Updates

---
+++ precache-manifest.e691476a233e4e7831b026c76abaeee4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb1a1bc4a5642a036aea9e90b3075fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64f32087b46d20d128ba",
+    "url": "/static/css/main.3c107d19.chunk.css"
+  },
+  {
+    "revision": "6a5a2a8a362a0bd426fd",
+    "url": "/static/js/2.200e75f0.chunk.js"
+  },
+  {
+    "revision": "3bd64847543c141cb60684ee9c4763a6",
+    "url": "/static/js/2.200e75f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64f32087b46d20d128ba",
+    "url": "/static/js/main.b29f08ae.chunk.js"
+  },
+  {
+    "revision": "afdabbf32c5500eedbc1",
+    "url": "/static/js/runtime-main.aa9822fd.js"
+  },
+  {
+    "revision": "c389c3ea46483a7bb9f3fce41b80d696",
+    "url": "/static/media/offer.c389c3ea.jpeg"
+  },
+  {
+    "revision": "e3bd6a7cb3a2428a6d0b1ced029ade3e",
+    "url": "/static/media/ogsl.e3bd6a7c.mp4"
+  },
+  {
+    "revision": "a313eddfb3dcbc2898f9d208053483d2",
+    "url": "/static/media/ogsl_1.a313eddf.jpg"
+  },
+  {
+    "revision": "92c27a233c9703901424b1423abfcb33",
+    "url": "/static/media/ogsl_2.92c27a23.jpg"
+  },
+  {
+    "revision": "ac7d6e369c5d5db7e3426258bdc407ad",
+    "url": "/static/media/ogsl_3.ac7d6e36.jpg"
+  },
+  {
+    "revision": "cbf8803996f1e5df16aa61b310e66d1b",
+    "url": "/static/media/ogsl_4.cbf88039.jpg"
+  },
+  {
+    "revision": "bd409438faa15c9c2caf6cc638cf100d",
+    "url": "/static/media/ogsl_6.bd409438.jpg"
+  },
+  {
+    "revision": "b53a22d517e4360efb3d9dd0ffa07386",
+    "url": "/static/media/ogsl_logo.b53a22d5.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ogsl1/precache-manifest.125a4c0e1c3c89d70ad1f321c208a002.js"
+  "/precache-manifest.e691476a233e4e7831b026c76abaeee4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ogsl1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
